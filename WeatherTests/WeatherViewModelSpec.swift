289326fc40470432f1ed5819ece45cdedf2ac895
Rafael Montilla <r.montilla.a@gmail.com>
2020-06-30 16:54:50 +0900

    Adding formatting unit test

--- WeatherTests/WeatherViewModelSpec.swift
+++ WeatherTests/WeatherViewModelSpec.swift
@@ -13,9 +13,21 @@ import Combine
 
 class MockAPI: API {
     var lastPath: String = ""
-    func makeRequest<T>(target: RequestConvertible, completion: @escaping (Result<T, APIError>) -> Void) where T : Decodable {
+    func makeRequest<T: Decodable>(target: RequestConvertible,
+                                   completion: @escaping (Result<T, APIError>) -> Void) {
         lastPath = target.path
-        return
+        
+        let result = Weather(country: "Japan",
+                             name: "Tokyo",
+                             temperature: 26.9,
+                             humidity: 83.4,
+                             pressure: 1023.2,
+                             precipitation: 4.2,
+                             weatherCondition: .lightRain,
+                             windDirection: .ESE,
+                             windSpeed: 4.2)
+        
+        completion(.success(result as! T))
     }
 }
 class MockLocationService: CoreLocationService {
@@ -35,7 +47,30 @@ class WeatherViewModelSpec: QuickSpec {
                     //Act
                     weatherViewModel.locationService.currentCoordinates.send(location)
                     //Assert
-                    expect(mockAPI.lastPath.hasPrefix("weather?")).to(equal(true))
+                    expect(mockAPI.lastPath.hasPrefix("weather?lat=1&lon=1")).to(equal(true))
+                }
+            }
+        }
+        
+        describe("api response") {
+            context("receives weather object") {
+                it("should format correctly") {
+                    //Arrange
+                    let mockAPI = MockAPI()
+                    let mockLocationService = MockLocationService()
+                    let weatherViewModel = WeatherViewModel(apiManager: mockAPI, locationService: mockLocationService)
+                    let location = Location(latitude: "1", longitude: "1")
+                    //Act
+                    weatherViewModel.locationService.currentCoordinates.send(location)
+                    //Assert
+                    expect(weatherViewModel.weatherImageName.value).to(equal(WeatherCondition.lightRain.dayImage))
+                    expect(weatherViewModel.formattedLocation.value).to(equal("Tokyo, Japan"))
+                    expect(weatherViewModel.weatherDescription.value).to(equal("26°C | \(WeatherCondition.lightRain.description)"))
+                    expect(weatherViewModel.formattedLocation.value).to(equal("Tokyo, Japan"))
+                    expect(weatherViewModel.humidity.value).to(equal("83.4%"))
+                    expect(weatherViewModel.precipitation.value).to(equal("4.2 mm"))
+                    expect(weatherViewModel.pressure.value).to(equal("1023.2 hPa"))
+                    expect(weatherViewModel.windSpeed.value).to(equal("4.2 km/h"))
                 }
             }
         }
